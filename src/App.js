bcd106c963b1d6557108f9a43db84a198f07f941
Kwaku Appau-Nkansah <brakwakujr@gmail.com>
2022-02-26 23:54:32 -0700

    App rewrittenand complete

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-// import { ChatEngine } from 'react-chat-engine';
-// import ChatFeed from './components/ChatFeed';
-// import './App.css';
+import { ChatEngine } from 'react-chat-engine';
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+import './App.css';
 
-// function App() {
-//   return (
-//     //chatengine.io/docs/functions
-//     <ChatEngine
-//      height='100vh'
-//      projectID='e50b504e-8770-48b5-aa85-0f3e62eeec20'
-//      userName='bot'
-//      userSecret='1234'
-//      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
-//     />
-//   );
-// }
+const projectID = 'e50b504e-8770-48b5-aa85-0f3e62eeec20';
 
-// export default App;
+function App() {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
+  return (
+    //https://chatengine.io/docs/react/v1
+    <ChatEngine
+     height='100vh'
+     projectID={projectID}
+     userName={localStorage.getItem('username')}
+     userSecret={localStorage.getItem('password')}
+     renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+     onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
+  );
+};
+
+// infinite scroll, logout, more customizations...
+
+export default App;
